cb45f0ffe04ce7a39adc058d9aa089dd98e9a1d3
Mignon Du Plessis <mignonduplessis@Mignons-MacBook-Pro.local>
2020-04-25 11:47:45 -0400

    logic update

--- static/logic.js
+++ static/logic.js
@@ -3,3 +3,33 @@ var gl = L.mapboxGL({
     accessToken: 'not-needed',
     style: 'https://api.maptiler.com/maps/3d26a01e-f289-425f-b403-d7737e1ae42f/style.json?key=G18kR4B5cKkYaH1F1cW3'
   }).addTo(map)
+
+
+var data = clinicalTrials;
+// console.log(Object.entries(data));
+console.log(data);
+
+// Create empty arrays to store the latitude
+var lat = [];
+
+
+// Iterate through each data object
+data.forEach((datapoint) => {
+
+  // Iterate through each key and value
+  Object.entries(datapoint).forEach(([key, value]) => {
+
+    // Use the key to determine which array to push the value to
+    if (key === "latitude") {
+      lat.push(value);
+    }
+    
+   });
+});
+
+// Use map to build array
+var latMapped = data.map(datapoint => data.latitude);
+
+
+console.log(latMapped);
+
