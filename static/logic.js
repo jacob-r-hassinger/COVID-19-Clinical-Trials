c9ddc365fc454c6e37665222ec9c45323633c820
Mignon Du Plessis <mignonduplessis@Mignons-MacBook-Pro.local>
2020-04-26 20:00:16 -0400

    fix

--- static/logic.js
+++ static/logic.js
@@ -1,33 +1,41 @@
-// //var gl = L.mapboxGL({
-//    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-//     accessToken: 'not-needed',
-//     style: 'https://api.maptiler.com/maps/3d26a01e-f289-425f-b403-d7737e1ae42f/style.json?key=G18kR4B5cKkYaH1F1cW3'
-//   }).addTo(map)
+
+
+
 
 
 var data = [clinicalTrials];
-// console.log(Object.entries(data));
-console.log(data);
+  // console.log(Object.entries(data));
+  console.log(data);
+
+
+// for (var i = 0; i < data.length; i++) {
+//     var covid = data[i];
+//     L.marker(covid.latitude, covid.longitude)
+//       .bindPopup("<h1>" + covid.facility + "</h1> <hr> <h3>Population " + covid.brieftitle + "</h3>")
+//       .addTo(map);
+//   }
 
 // Create empty arrays to store the latitude
-lats = []; 
+lats = [];
 longs = [];
 names = [];
 titles = [];
-statuses = [];
-phases = [];
 for (i = 0; i < clinicalTrials.length; i++) {
-  lats.push(clinicalTrials[i]["latitude"]);
-  longs.push(clinicalTrials[i]["longitude"]);
-  titles.push(clinicalTrials[i]["brieftitle"]);
-  names.push(clinicalTrials[i]["facility"]);
-  statuses.push(clinicalTrials[i]["overallstatus"]);
-  phases.push(clinicalTrials[i]["phase"]);
+    lats.push(clinicalTrials[i]["latitude"]);
+    longs.push(clinicalTrials[i]["longitude"]);
+    titles.push(clinicalTrials[i]["brieftitle"]);
+    names.push(clinicalTrials[i]["facility"]);
 };
 
-// Use map to build array
-var latMapped = data.map(datapoint => data.latitude);
 
+var gl = L.mapboxGL({
+  attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  accessToken: 'not-needed',
+  style: 'https://api.maptiler.com/maps/3d26a01e-f289-425f-b403-d7737e1ae42f/style.json?key=G18kR4B5cKkYaH1F1cW3'
+}).addTo(map)
 
-console.log(latMapped);
+var marker = L.marker([lats, longs], {
+    draggable: true,
+    title: "My First Marker"
+}).addTo(map);
 
