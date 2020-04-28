a3f7a83abc9a4bbcf550c7f19353cd424c38917d
Jake Hassinger <jacob.r.hassinger@gmail.com>
2020-04-27 20:50:47 -0400

    adding updates for plot 1 the bar chart

--- static/plot1.js
+++ static/plot1.js
@@ -1,9 +1,18 @@
 var plotdata = clinicalTrials[1];
 console.log(plotdata)
 
+yVals = [];
+xVals = [];
+
+for (var i = 0; i < plotdata.length; ++i) {
+  yVals.push(plotdata[i]["count"]);
+  xVals.push(plotdata[i]["locationstate"]);
+  console.log((plotdata[i]["locationstate"]))
+};
+
 var trace1 = {
-  x: [],
-  y: [],
+  x: xVals,
+  y: yVals,
   type: "bar"
 };
 
