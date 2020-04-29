cd349c6f8d34047483665fdea9489b55da44eb2e
Mignon Du Plessis <mignonduplessis@Mignons-MacBook-Pro.local>
2020-04-29 19:35:14 -0400

    style changes

--- static/logic.js
+++ static/logic.js
@@ -13,19 +13,30 @@ L.tileLayer('https://api.maptiler.com/maps/positron/{z}/{x}/{y}.png?key=G18kR4B5
 var data = clinicalTrials[0];
 console.log(data);
 
+var myURL = ("https://i.ibb.co/VNqwM47/icon-512x512-2x.png")
+
+var myIcon = L.icon({
+  iconUrl: myURL,
+  iconSize: [48, 48]
+});
 
 var markerClusters = L.markerClusterGroup();
 
 for (var i = 0; i < data.length; ++i) {
-  var popup = data[i].facility +
+  var popupContent = 
+    '<br/><b>FACILITY:<b>   ' + data[i].facility +
     '<br/><b>Brief Title:<b>' + data[i].brieftitle +
     '<br/><b>Enrollment Count:</b> ' + data[i].enrollmentcount +
     '<br/><b>Contact Name:</b> ' + data[i].contactname +
     '<br/><b>Contact Phone:</b> ' + data[i].contactphone;
 
-
-  var m = L.marker([data[i].latitude, data[i].longitude])
-    .bindPopup(popup);
+    var popupOptions =
+    {
+      'maxWidth': '500',
+      'className': 'another-popup'
+    }
+  var m = L.marker([data[i].latitude, data[i].longitude], { icon: myIcon })
+    .bindPopup(popupContent, popupOptions);
 
   markerClusters.addLayer(m);
 }
